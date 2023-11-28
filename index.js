e033df601164f1e2ab1dd6c0dc938edb101724ec
tazbir01 <tajberahamad@gmail.com>
2023-11-28 08:38:50 +0600

    added get method for users

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/users", async(req,res)=>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
