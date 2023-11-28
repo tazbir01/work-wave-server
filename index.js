4ac8a7fb0519e242a2816d168381956a3e066d3e
tazbir01 <tajberahamad@gmail.com>
2023-11-28 18:44:34 +0600

    added jwt and middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -28,6 +29,32 @@ async function run() {
     const reviewCollection = client.db('workwaveDB').collection('reviews')
     const userCollection = client.db('workwaveDB').collection('users')
 
+    // jwt related api
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {
+        expiresIn: '1hr'
+      })
+      res.send({token})
+    })
+
+    // middleware
+    const verifyToken = (req,res, next)=>{
+      console.log("Indise verify token", req.headers)
+      if(!req.headers.authorization){
+        return res.status(401).send({message: "unauthorized access"})
+      }
+      const token = req.headers.authorization.split('')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+        if(error){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded
+        next()
+      })
+    }
+
+    // review related api
     app.get('/reviews', async(req,res)=>{
         const result = await reviewCollection.find().toArray()
         res.send(result)
@@ -38,7 +65,7 @@ async function run() {
       const user = req.body
 
       const query = {email: user.email}
-      const isExistingUser = await userCollection.findOne().toArray(query)
+      const isExistingUser = await userCollection.findOne(query)
       if(isExistingUser){
         res.send({ message: "user already exists", insertedId: null})
       }
