c1dc2967841406f38f5d6b99a341dd3aece309a7
tazbir01 <tajberahamad@gmail.com>
2023-12-04 15:36:49 +0600

    verify token for user

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function run() {
     })
 
 
-    app.get('/users/:id', async (req, res) => {
+    app.get('/users/:id',verifyToken, async (req, res) => {
       const id = req.params.id
       const filter = { _id: new ObjectId(id) }
       const result = await userCollection.findOne(filter)
@@ -239,7 +239,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
